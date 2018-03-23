6f482f07272dba293538040354a8b22b7eec6bd6
zyxelva <zyxelva@gmail.com>
2018-03-23 09:51:54 +0800

    优化数据源的配置，读取配置的数据库文件；

--- src/com/zyx/test/java/UserServiceTest.java
+++ src/com/zyx/test/java/UserServiceTest.java
@@ -15,6 +15,6 @@ public class UserServiceTest extends SpringTestCase {
     @Test
     public void selectUserByIdTest(){
         SysUser user = userService.selectByPrimaryKey(1);
-        System.out.println(user.getUsername() + ":" + user.getPassword());
+        System.out.println(user.getUsername() + "+++++++++++++++++++++++++++:" + user.getPassword());
     }
 }
